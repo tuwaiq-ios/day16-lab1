2b2c2d5fdc9bc6e35c77e6d15031ee1d794c1e43
saram <Salshahrani153@gmail.com>
2021-10-17 13:41:36 +0300

    finish

---
+++ Day 16/Day 16/ToDo/Taskcell.swift
@@ -0,0 +1,43 @@
+//
+//  Taskcell.swift
+//  Day 16
+//
+//  Created by Sara M on 11/03/1443 AH.
+//
+
+
+import UIKit
+protocol checkButtonDelegate {
+    func checkTapped(at indx: IndexPath)
+}
+
+class Taskcell: UITableViewCell, UITextFieldDelegate {
+
+  
+    @IBOutlet weak var label1: UITextField!
+    var indexpath: IndexPath!
+    var taskTitle: checkButtonDelegate!
+   
+    @IBOutlet weak var imagecheck: UIImageView!
+    
+    var isChecked = false
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        label1.delegate = self
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            let update = Task(taskTitle: label1.text, isChecked: taskArray[indexpath.row].isChecked)
+            
+            taskArray[indexpath.row] = update
+        label1.resignFirstResponder()
+            return true
+        }
+    }
