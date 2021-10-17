1f767989c075a0a8d83a11ac2f0637bb121fbc46
amal <amal.141726@gmail.com>
2021-10-17 15:22:06 +0300

    finish

---
+++ day16/tasbih/Tasbih.swift
@@ -0,0 +1,35 @@
+//
+//  Tasbih.swift
+//  day16
+//
+//  Created by Amal on 11/03/1443 AH.
+//
+
+import UIKit
+
+class Tasbih: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+ 
+    }
+ var currentcounter = 0
+    @IBOutlet weak var label: UILabel!
+    
+    func reset(){
+        currentcounter = 0
+        label.text = String(describing: currentcounter)
+    }
+    
+    @IBAction func resett(_ sender: Any) {
+    reset()
+    }
+    
+    func count(){
+        currentcounter += 1
+        label.text = String(describing: currentcounter)
+    }
+    
+    @IBAction func coun(_ sender: Any) {
+    count()
+    }
+}
