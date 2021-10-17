8e6b32c380dceb98ac64eb9b2342e4fd36eddee8
hanan asiri <kayan222276@yahoo.com>
2021-10-17 15:12:14 +0300

    Done

---
+++ day16-Hanan/day16-Hanan/Jahiz/Cell1.swift
@@ -0,0 +1,18 @@
+//
+
+import UIKit
+
+class Cell1: UITableViewCell {
+    
+    @IBOutlet var restLogo: UIImageView!
+    
+    @IBOutlet var restName: UILabel!
+    
+    
+    @IBOutlet var restType: UILabel!
+    
+    
+    @IBOutlet var restRating: UILabel!
+    
+    @IBOutlet var restState: UILabel!
+}
---
+++ day16-Hanan/day16-Hanan/Jahiz/Cell2.swift
@@ -0,0 +1,11 @@
+
+import UIKit
+
+class Cell2: UITableViewCell {
+    
+    @IBOutlet var foodImage: UIImageView!
+    
+    @IBOutlet var foodName: UILabel!
+    
+    @IBOutlet var foodPrice: UILabel!
+}
