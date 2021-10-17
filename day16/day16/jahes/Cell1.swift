4c808258b1029b197a4b4cbda25a23d1b8a79828
PC <alahmryn5@gmail.com>
2021-10-17 14:06:44 +0300

    done3project

---
+++ day16/day16/jahes/Cell1.swift
@@ -0,0 +1,19 @@
+//
+//  Viewcontrollerjahes.swift
+//  day16
+//
+//  Created by PC on 11/03/1443 AH.
+//
+
+import Foundation
+import UIKit
+
+class Cell1: UITableViewCell {
+    
+    @IBOutlet weak var restLogo: UIImageView!
+    @IBOutlet weak var restName: UILabel!
+    @IBOutlet weak var restType: UILabel!
+    @IBOutlet weak var restState: UILabel!
+    @IBOutlet weak var restRating: UILabel!
+}
+
