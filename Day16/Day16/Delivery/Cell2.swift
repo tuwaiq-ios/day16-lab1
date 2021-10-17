bc524351a31a40e39945edcc39896de227db8fb1
dmdm <deemamoh15@hotmail.com>
2021-10-17 15:20:26 +0300

    done

---
+++ Day16/Day16/Delivery/Cell2.swift
@@ -0,0 +1,18 @@
+//
+//  Cell2.swift
+//  Day16
+//
+//  Created by dmdm on 17/10/2021.
+//
+
+import UIKit
+
+class Cell2: UITableViewCell {
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    
+    @IBOutlet weak var foodName: UILabel!
+    
+    @IBOutlet weak var foodPrice: UILabel!
+    
+}
---
+++ Day16/Day16/Delivery/VC1.swift
@@ -0,0 +1,58 @@
+//
+//  VC1.swift
+//  Day16
+//
+//  Created by dmdm on 17/10/2021.
+//
+import UIKit
+
+class VC1: UIViewController, UISceneDelegate, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rest = restList[indexPath.row]
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "RestCell",
+            for: indexPath) as! Cell1
+        
+        cell.restName.text = rest.name
+        cell.restLogo.image = UIImage(named: rest.logo)
+        cell.restRating.text = "\(rest.rating)"
+        cell.restState.text = rest.state
+        cell.restType.text = rest.type
+        
+        return cell
+    }
+    
+    @IBOutlet weak var restTV: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        restTV.delegate = self
+        restTV.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rest = restList[indexPath.row]
+        
+        let oldValue = counter[rest.name] ?? 0
+        counter [rest.name] = oldValue + 1
+        
+        performSegue(
+            withIdentifier: "show_menu",
+            sender: rest)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        
+        let vc2 = segue.destination as! VC2
+       
+        vc2.restFromVC1 = sender as? Rest
+
+    }
+    
+}
