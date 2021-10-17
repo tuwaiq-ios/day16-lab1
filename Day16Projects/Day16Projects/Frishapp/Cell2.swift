54e8f908fc4a8ae7f9fcc60ac534cc562b743ff6
HANAN <hanan@HANANMacBook-Pro.local>
2021-10-17 15:23:04 +0300

    finish

---
+++ Day16Projects/Day16Projects/Frishapp/Cell2.swift
@@ -0,0 +1,16 @@
+//
+//  Cell2.swift
+//  Day16Projects
+//
+//  Created by HANAN on 11/03/1443 AH.
+//
+
+import UIKit
+
+class Cell2: UITableViewCell {
+    
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var foodName: UILabel!
+    @IBOutlet weak var foodPrice: UILabel!
+}
+
