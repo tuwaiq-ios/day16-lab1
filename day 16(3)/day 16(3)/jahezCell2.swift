9a4283a34ddb86fe12b8af9ca9e6eb4c9b9e113b
alanood <alanood@MacBook-Air-alanood.local>
2021-10-18 16:34:51 +0300

    day 16

---
+++ day 16(3)/day 16(3)/jahezCell2.swift
@@ -0,0 +1,15 @@
+//
+//  jahezCell2.swift
+//  day 16(3)
+//
+//  Created by alanood on 11/03/1443 AH.
+//
+
+import UIKit
+class jahezCell2: UITableViewCell {
+    
+    @IBOutlet weak var foodPrice: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var foosName: UILabel!
+}
+
---
+++ day 16(3)/day 16(3)/jahezVC2.swift
@@ -0,0 +1,60 @@
+//
+//  jahezVC2.swift
+//  day 16(3)
+//
+//  Created by alanood on 11/03/1443 AH.
+//
+
+import UIKit
+class jahezVC2: UIViewController ,UITableViewDelegate ,UITableViewDataSource {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restFromVC1?.foods.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       
+        let food = restFromVC1?.foods[indexPath.row]
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier:"Foodcell",
+            for: indexPath
+        ) as! jahezCell2
+        
+        cell.foosName.text = food?.name
+        cell.foodPrice.text = "\(food!.price)"
+        cell.foodImage.image = UIImage(named: food!.image)
+        
+        return cell
+    }
+    
+    
+    var restFromVC1: Rest?
+    
+    @IBOutlet weak var menuTV: UITableView!
+    @IBOutlet weak var restName: UILabel!
+    @IBOutlet weak var restType: UILabel!
+    @IBOutlet weak var restState: UILabel!
+    @IBOutlet weak var restLogo: UIImageView!
+    @IBOutlet weak var restRating: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        restName.text = restFromVC1?.name
+        restType.text = restFromVC1?.type
+        restState .text = restFromVC1?.state
+        restLogo.image = UIImage(named: restFromVC1!.logo)
+        restRating.text = "\(restFromVC1!.rating)"
+        
+        Counterpress.text = "\( counter[restFromVC1!.name]!)"
+        
+        
+        
+        menuTV.delegate = self
+        menuTV.dataSource = self
+    }
+    @IBOutlet weak var Counterpress: UILabel!
+}
+
+
